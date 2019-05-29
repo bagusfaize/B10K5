8390916805ea81a41392e8ed66e3247d416fb0b8
Bagus Faiz <50720858+bagusfaizn@users.noreply.github.com>
2019-05-29 23:40:00 +0700

    update struktur ui baru index.php

--- 6B/index.php
+++ 6B/index.php
@@ -4,24 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Arkademy</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <title>Database Management App</title>
+    <link href="https://fonts.googleapis.com/css?family=Muli:200,300,400,600,800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <div class="container pt-5">
+    <div class="container py-5">
         <div class="d-flex align-items-center justify-content-center h-100">
         <div class="d-flex flex-column">
+        <p class="h4">Database Management App</p>
             <!-- INPUT PROGRAMMER -->
-            <div class="box px-5 py-3 my-5">
-            <form method="post" action="input_programmer.php" class="form-inline">
-            <input type="text" name="name" class="form-control mx-3 w-75" placeholder="Input nama programmer">
-            <input class="btn btn-info" type="submit">
-            </form>
+            <form action="input_programmer.php" method="post">
+            <div class="box px-5 py-3 my-5 w-100">
+                <div class="row justify-content-between">
+                    <div class="col-9">
+                    <input type="text" name="name" class="form-control mx-3 w-100" placeholder="Input programmer's name">
+                </div>
+                    <div class="col">
+                    <input class="btn btn-bulet float-right mr-3" type="submit" value="Add">
+                    </div>
+                </div>
             </div>
+            </form>
             <!-- TAMPILAN DATABASE -->
-
             <?php
             include 'koneksi.php';
             $query = "SELECT users.id, (users.name) AS programmer, (skills.name) AS skill
@@ -32,46 +38,36 @@
             while($data = mysqli_fetch_assoc($sql)){
             $pk=$data['id'];
             ?>           
-            <div class="box px-5 py-3 my-1 w-100">
-            <table class="table my-3">
-            <tr>
-                <td class="w-50"><?php echo $data['programmer']?></td>
-                <td>
-                    <form method="post" action="input_skill.php" class="form-inline">
-                    <span class="mt-4">
-                    <input type="text" class="form-control mx-2 w-50" name="name" placeholder="Input skill">
+            <div class="box px-5 py-4 my-2 w-100">
+                <div class="row justify-content-between">
+                    <div class="col-7">
+                        <span class="d-block nama my-1"><?php echo $data['programmer']?></span>
+                        <span class="skill"> Skills : 
+                        <?php
+                        include 'koneksi.php';
+                        $queryskill = "SELECT * FROM skills where user_id = '$pk'";
+                        $keahlian = mysqli_query($koneksi, $queryskill);
+                        while($list = mysqli_fetch_array($keahlian)){
+                            ?>
+                        <?php echo $list['name']."," ?>
+                        <?php
+                        }                
+                        ?>   
+                        </span>
+                        </div>
+                    <div class="col">                    
+                    <form method="post" action="input_skill.php" class="form-inline ml-3 mt-2">
+                    <input type="text" class="form-control mx-2 w-50" name="name" placeholder="Input skills">
                     <input type="text" value="<?php echo $data['id']?>" name="id" class="form-control mx-2 w-25" style="display:none;">
-                    <input type="submit" class="btn btn-info" placeholder="Tambah">
-                    </span>
+                    <input type="text" value="<?php echo $data['id']?>" name="id" class="form-control mx-2 w-25" style="display:none;">
+                    <input class="btn btn-bulet float-right" type="submit" value="Add">
+                    </div>
                     </form>
-                </td>
-            </tr>
-            <tr>
-            <td>
-                <?php
-                include 'koneksi.php';
-                $queryskill = "SELECT * FROM skills where user_id = '$pk'";
-                $keahlian = mysqli_query($koneksi, $queryskill);
-                while($list = mysqli_fetch_array($keahlian)){
-                    ?>
-                <?php echo $list['name']."," ?>
-                <?php
-                }                
-                ?>                
-            </td>
-            </tr>
-            </table>
+                </div>
             </div>
-
             <?php
             }
             ?>
-
-        </div>
-        </div>
-    </div>
 </div>
 </body>
 </html>
-
-
